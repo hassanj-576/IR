4d7039f1cb2106996dd460d1438991587bc7e12d
Hassan Jalil <11besesjalil@seecs.edu.pk>
2015-01-11 01:47:28 +0500

    Code with a simple IR code and random transmit

---
+++ IR/src/com/example/ir/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.ir;
+
+import java.io.Console;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.ConsumerIrManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+
+public class MainActivity extends Activity{
+	ConsumerIrManager mCIR;	
+	private static final String TAG = "ConsumerIrTest";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button irButton = (Button) findViewById(R.id.IrButton);
+        mCIR = (ConsumerIrManager)getSystemService(Context.CONSUMER_IR_SERVICE);
+       
+        irButton.setOnClickListener(new OnClickListener() {
+        	
+       
+			public void onClick(View v) {
+				 if (!mCIR.hasIrEmitter()) {
+	        		 Log.e(TAG, "No IR Emitter found\n");
+	        		 return;
+	        		 }
+				 int[] pattern = {1901, 4453, 625, 1614, 625, 1588, 625, 1614, 625, 442, 625, 442, 625,
+						 468, 625, 442, 625, 494, 572, 1614, 625, 1588, 625, 1614, 625, 494, 572, 442, 651,
+						 442, 625, 442, 625, 442, 625, 1614, 625, 1588, 651, 1588, 625, 442, 625, 494, 598,
+						 442, 625, 442, 625, 520, 572, 442, 625, 442, 625, 442, 651, 1588, 625, 1614, 625,
+						 1588, 625, 1614, 625, 1588, 625, 48958};
+						 // transmit the pattern at 38.4KHz
+						 mCIR.transmit(38400, pattern);
+			}
+		});
+      
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+	
+}
